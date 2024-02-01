1521eb82d62cd3bad8d4db40ce16b9ef49f764e7
ElijahSemrau <elijahsemrau@gmail.com>
2024-02-01 04:32:05 -0600

    Finished rework and additions
    Added DOM components and CSS

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Rock_Paper_Scissors
 
-Rock Paper Scissors for Odin Project's first Javascript project
+Rock Paper Scissors for Odin Project's first Javascript project.
+
+Player chooses an option, the computer's option is randomly chosen.
+
+First to 5 wins is crowned champion.
--- rps.js
+++ rps.js
@@ -3,24 +3,34 @@ let comp;
 let player;
 let playerWins = 0;
 let computerWins = 0;
+let winFlag = 'false';
 
-//Allows the creation of a random full number integerRock
+//Creates elements with reference links
+const container = document.querySelector('#container');
+const results = document.querySelector('#results');
+const runningScore = document.querySelector('#runningScore');
+const winnerMessage = document.querySelector('#winnerMessage');
+
+//Creates elements
+var choices = document.createElement("choices");
+var roundResults = document.createElement("roundResults");
+var roundScore = document.createElement("roundScore");
+var winner = document.createElement("winner");
+
+//Allows the creation of a random full number integer
 function getRandomInt(max){
     return Math.floor(Math.random() * max);
 }
 
-//Requests, validates, and returns the player choice
-function getPlayerChoice() {
-    player = prompt("Choose an option: Rock, Paper, or Scissors:")
-    player = player[0].toUpperCase() + player.slice(1).toLowerCase();
-
-    while (player !== "Rock" && player !== "Scissors" && player !== "Paper") {
-        player = prompt("Invalid choice. Choose an option: Rock, Paper, or Scissors:");
-        player = player[0].toUpperCase() + player.slice(1).toLowerCase();
-    }
-
-    return player;
-}
+//Creates the button selection, and takes the text for the button and logs it as the player option.
+const buttons = document.querySelectorAll('.playerOption')
+buttons.forEach(button => {
+    button.addEventListener('click', function(event){
+        console.log(event.target.textContent);
+        player = event.target.textContent;
+        playRound();
+    });
+});
 
 
 
@@ -40,82 +50,95 @@ function getComputerChoice () {
 
 //Function for a single round
 function playRound() {
-    getPlayerChoice();
+
+    //calls getComputerChoice at the end of the round to compare to player choice
     getComputerChoice();
 
+
     //Prints out each of the choices
-    console.log("Player has chosen", player);
-    console.log("Computer has chosen", comp);
+    choices.classList.add('choices');
+    choices.textContent = 'You have chosen ' + player + ' and the Computer has chosen ' + comp + '!';
+    container.appendChild(choices);
+ 
+
 
     //Prints out if there is a tie
     if (player == comp) {
-        console.log("The player and the computer have tied with", player);
+        roundResults.classList.add('roundResults');
+        roundResults.textContent = 'You and the computer have tied with '+ player + '!';
+        results.appendChild(roundResults);
     }
 
     //Prints if the player wins with Rock
     if (player == "Rock" && comp == "Scissors") {
-        console.log("The player has won!");
+        roundResults.classList.add('roundResults');
+        roundResults.textContent = 'You won!';
+        results.appendChild(roundResults);
         playerWins++;
     }
 
     //Prints if the player loses with Rock
     if (player == "Rock" && comp == "Paper") {
-        console.log("The computer has won!");
+        roundResults.classList.add('roundResults');
+        roundResults.textContent = 'The Computer has won!';
+        results.appendChild(roundResults);
         computerWins++;
     }
 
     //Prints if the player wins with Paper
     if (player == "Paper" && comp == "Rock") {
-        console.log("The player has won!");
+        roundResults.classList.add('roundResults');
+        roundResults.textContent = 'You won!';
+        results.appendChild(roundResults);
         playerWins++;
     }
 
     //Prints if the player loses with Paper
     if (player == "Paper" && comp == "Scissors") {
-        console.log("The computer has won!");
+        roundResults.classList.add('roundResults');
+        roundResults.textContent = 'The Computer has won!';
+        results.appendChild(roundResults);
         computerWins++;
     }
 
     //Prints if the player wins with Scissors
     if (player == "Scissors" && comp == "Paper") {
-        console.log("The player has won!");
+        roundResults.classList.add('roundResults');
+        roundResults.textContent = 'You won!';
+        results.appendChild(roundResults);
         playerWins++;
     }
 
     //Prints if the player loses with Scissors
     if (player == "Scissors" && comp == "Rock") {
-        console.log("The computer has won!");
+        roundResults.classList.add('roundResults');
+        roundResults.textContent = 'The Computer has won!';
+        results.appendChild(roundResults);
         computerWins++;
     }
 
-    console.log("The player has", playerWins,"points and the computer has", computerWins,"points.")
+    roundScore.classList.add('roundScore');
+    roundScore.textContent = 'You have ' + playerWins + ' points and the Computer has ' + computerWins + " points!";
+    runningScore.appendChild(roundScore);
 
-    return getPlayerChoice, getComputerChoice;
+    winner.classList.add('winner');
+
+    if (playerWins === 5 && winFlag === 'false') {
+        winner.textContent = '👑You have been crowned winner after five wins!👑';
+        winnerMessage.appendChild(winner);
+        winFlag = 'true';
+    }
+
+    if (computerWins === 5 && winFlag === 'false') {
+        winner.textContent = '👑The Computer has been crowned winner after five wins!👑';
+        winnerMessage.appendChild(winner);
+        winFlag = 'true';
+    }
+    
+
+    return player, getComputerChoice;
   }
 
-  //Function to play the game and print win/loss records
-  function game(){
-
-    //Loops for 5 rounds
-    for(let i = 0; i < 5; i++ ){
-        playRound();
-        //If there is a tie, redoes the round
-        if (player == comp){
-            i--;
-        }
-    }
-
-    if (playerWins > computerWins) {
-        console.log("The player has won", playerWins,"-",computerWins,"!");
-    }
-
-    if (playerWins < computerWins) {
-        console.log("The computer has won", computerWins,"-",playerWins,"!");
-    }
-  }
-
-  //Starts the game
-  game();
 
 
 
